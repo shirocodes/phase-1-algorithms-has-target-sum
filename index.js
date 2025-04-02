574b8a642421113dfc6b0e843dc42ce8516a39d8
shirocodes <ishirapt@gmail.com>
2025-04-02 14:05:46 +0300

    Completed the lab

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
+// function hasTargetSum(array, target) {
+  // iterate through each number in array
+//   for (let i = 0; i < array.length; i++) {  //n steps
+//     //for the current number, identify a complement (target - number) adding to target
+//     const complement = target - array[i]
+//      //iterate through the rest of array
+//      for (let j = i +1; j < array.length; j++) { //n*n steps
+//       //check if any number is complementary
+//       //if so return true, else, false
+//       if (array[j] === complement) return true
+//      }
+//   }
+//   return false;
+// }
+
+//optimize code
+  //create object to put numbers we've seen
+  //check if any key is complement > true false
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+ const seenNumbers = {}
+ for (let i = 0; i< array.length; i++) {  
+  const complement = target - array[i]
+
+  if(seenNumbers[complement]) return true
+  seenNumbers[array[i]] = true
 }
+return false
+}
+
+  // Write your algorithm here
+          /*
+          the function receives an array of integers, and from that array,
+          summing up two integers in the array adds up to the target number
+
+          we need a variable defining how to find the pair and declare true/false
+          */
 
 /* 
   Write the Big O time complexity of your function here
+runtime: O(n*2)
+space: 0(n) 
+
 */
 
 /* 
   Add your pseudocode here
+  function hasTargetSum(array, targetNumber)
+    iterate through each number in arrar
+    for the current number, identify a complement (target - number) adding to target
+    iterate through the rest of array
+      check if any number is complementary
+      if so return true, else, false
 */
 
 /*
@@ -26,6 +68,8 @@ if (require.main === module) {
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
   console.log("");
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 4, 3, 6], 5));
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
